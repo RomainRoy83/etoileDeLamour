3767c35565f8ed16d6d92773e78d33cd23eb8ea8
LAURE CARILLO <laure.carillo@gmail.com>
2022-06-03 00:23:20 +0200

    Header ok - début de la homepage

--- src/components/js/Header.js
+++ src/components/js/Header.js
@@ -4,11 +4,14 @@ import "../css/Header.css"
 const Header = () => {
   return (
     <header className="header">
+      <Link to="/"><img src="https://cdn.pixabay.com/photo/2021/03/22/22/06/couple-6115830_960_720.jpg" alt = "Dark Lovers" className="home"/></Link>
+      
+
+
       <nav className="header-links">
-        <Link to="/">Home</Link>
-        <Link to="/DarkSide">darkside</Link>
-        <Link to="/LightSide">LightSide</Link>
-        <Link to="/Teaser">Teaser</Link>
+        <Link to="/DarkSide"><img src="https://cdn.pixabay.com/photo/2020/05/31/23/54/luna-5244609_960_720.jpg"className="dark"/></Link>
+        <Link to="/LightSide"><img src="https://cdn.pixabay.com/photo/2016/08/12/02/10/space-1587393_960_720.jpg"className="light"/></Link>
+
       </nav>
     </header>
   );
--- src/screens/js/HomePage.js
+++ src/screens/js/HomePage.js
@@ -1,8 +1,37 @@
+import { Link } from "react-router-dom";
+import { HomeCss } from "../css/HomePage.css";
+
 const Home = () => {
   return (
-    <div className="home">
-      <h1>Welcome to the home page !</h1>
-    </div>
+  <>
+<section class="star-wars">
+
+<div class="crawl">
+  
+  <div class="title">
+    <p>Votre destinée vous attend!</p>
+    <h1>L'Etoile de l'Amour est là!</h1>
+  </div>
+  
+    <p>Tout comme sa grande sœur, l’Étoile de l’Amour frappe toujours juste. Trouvez cet être spécial qui manque à votre vie.</p>     
+    <p>Que vous soyez en quête d’un beau marchand Hutt, d’un chasseur de primes Twi’lek ou d’un Jedi humain, l’étoile de l’amour fera exploser votre cœur d’émotion.</p>
+    <p>L’amour n’a pas de frontière, pas même celle de la bordure extérieure...</p>
+    <img src="https://cdn.pixabay.com/photo/2020/02/07/19/05/spaceship-4828098_960_720.jpg" alt="Vaisseau"/>
+  </div>   
+</section>
+<section>
+<div>
+    <h1>Notre Questionnaire de l'amour</h1>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p> 
+      <p>standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      <p> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+      <p> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,</p>
+      <p>and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+    
+  </div>
+  
+</section>
+</>
   );
 };
 
