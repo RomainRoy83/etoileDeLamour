d5c7cb711ecb29601e0989ec31114871ff202b4d
ANDREA GOMEZ <gomezandrea@hotmail.fr>
2022-06-03 02:46:52 +0200

    bugFixRoute

---
+++ src/screens/js/LightSide.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CruisersProfiles from "../../components/js/CruisersProfiles";
+
+
+function LightSide() {
+  return (
+    <div className="CroisiereLove">
+      <CruisersProfiles version="Resistance" />
+    </div>
+  );
+}
+
+export default LightSide;
